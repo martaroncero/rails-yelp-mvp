e075bf9c594e30829d31ab733b92019686104034
Marta Roncero <m.ronceroolmeda@gmail.com>
2021-02-17 15:27:51 +0000

    Created views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,19 @@
 class ReviewsController < ApplicationController
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id]) # restaurant for the form
     @review = Review.new # needed to instantiate the form_for
   end
 
   def create
-    @review = Review.new(restaurant_params)
-    @review.save
-
-    # no need for app/views/restaurants/create.html.erb
-    redirect_to review_path(@review)
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id]) # restaurant for the review
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render(:new) # render the new.html.erb
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-#   #a visitor can see the list of all restaurants
-#     get "restaurants", to: "restaurants#index"
+  #a visitor can see the list of all restaurants
+    get "restaurants", to: "restaurants#index"
 
-#   #A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
-#     get "restaurants/new", to: "restaurants#new"
-#     post "restaurants", to: "restaurants#create"
+  #A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+    get "restaurants/new", to: "restaurants#new"
+    post "restaurants", to: "restaurants#create"
 
-#  #A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
-#     get "restaurants/:id", to: "restaurants#show", as: :restaurant
-
-# #A visitor can add a new review to a restaurant
-#     resources :restaurants do
-#     resources :reviews, only: [ :new, :create ]
+ #A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+    get "restaurants/:id", to: "restaurants#show", as: :restaurant
 
     resources :restaurants do
     # GET restaurants/1/reviews/new new
@@ -21,5 +17,4 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
 
   end
-  resources :reviews, only: :destroy
 end
